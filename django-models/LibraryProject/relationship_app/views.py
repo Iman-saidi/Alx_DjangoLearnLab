49f3cdd628d43d54bf6edbf22d60c613e4ac3502
Iman-saidi <summeiyaiman@gmail.com>
2025-07-20 21:07:25 +0300

    django-models

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,34 +1,13 @@
 from django.shortcuts import render, redirect
-from .models import Book
-from django.views.generic.detail import DetailView
-from .models import Library
-
-from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.auth.forms import UserCreationForm
-from django.views.generic.edit import CreateView
-from django.urls import reverse_lazy
+from django.contrib.auth.views import LoginView, LogoutView
 
-# Create your views here.
-# Function based view
-def list_books(request):
-    books = Book.objects.all()
-    context = {'list_books': books}
-    return render(request, 'relationship_app/list_books.html', context)
-
-# Class based view
-class LibraryDetailView(DetailView):
-    model = Library
-    template_name = 'relationship_app/library_detail.html'
-    context_object_name = 'library'
-
-# Authentication
-class LoginView(LoginView):
-    template_name = 'relationship_app/login.html'
-
-class LogoutView(LogoutView):
-    template_name = 'relationship_app/logout.html'
-
-class RegisterView(CreateView):
-    form_class = UserCreationForm
-    template_name = 'relationship_app/register.html'
-    success_url = reverse_lazy('login')
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')  # Redirect to login after successful registration
+    else:
+        form = UserCreationForm()
+    return render(request, 'relationship_app/register.html', {'form': form})
