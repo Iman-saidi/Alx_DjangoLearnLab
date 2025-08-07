f058f8df8e07761afcb9b07f0a4962491badaf1e
Iman-saidi <summeiyaiman@gmail.com>
2025-08-07 15:10:58 +0300

    api_project

--- api_project/api_project/urls.py
+++ api_project/api_project/urls.py
@@ -1,22 +1,15 @@
-"""
-URL configuration for api_project project.
+# api_project/urls.py
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.http import HttpResponse
+
+# Optional: a simple landing page at /
+def home(request):
+    return HttpResponse("<h1>Welcome to the A.D.Ai API</h1><p>Go to <a href='/api/'>/api/</a> to access the API.</p>")
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', home),                         # http://127.0.0.1:8000/
+    path('admin/', admin.site.urls),        # Admin
+    path('api/', include('api.urls')),      # Your app-level URLs
 ]
