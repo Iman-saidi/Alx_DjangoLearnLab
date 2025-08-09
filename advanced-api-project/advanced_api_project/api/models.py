d462646aa3fcef17526bea9cbbf540c53320b491
Iman-saidi <summeiyaiman@gmail.com>
2025-08-09 14:18:13 +0300

    advanced-api-project

--- advanced-api-project/advanced_api_project/api/models.py
+++ advanced-api-project/advanced_api_project/api/models.py
@@ -1,17 +1,16 @@
 from django.db import models
 
-# This file defines the data models for the API project.
+# Create your models here.
 class Author(models.Model):
     name = models.CharField(max_length=100)
-    
+    # email = models.EmailField(unique=True)
 
     def __str__(self):
         return self.name
-    
 class Book(models.Model):
     title = models.CharField(max_length=200)
-    author = models.ForeignKey(Author, related_name='books', on_delete=models.CASCADE)
     publication_year = models.IntegerField()
-
+    auther = models.ForeignKey(Author, related_name='books', on_delete=models.CASCADE)
+    
     def __str__(self):
         return self.title
--- advanced-api-project/advanced_api_project/api/serializers.py
+++ advanced-api-project/advanced_api_project/api/serializers.py
@@ -1,26 +1,19 @@
 from rest_framework import serializers
-from .models import Book, Author
 from datetime import datetime
-
-# This file defines the serializers for the API project.
+from .models import Author, Book
+                          
 class BookSerializer(serializers.ModelSerializer):
-    
     class Meta:
-        model = Book
-        fields = ['id', 'title', 'author', 'publication_year']  
-
-
-    # This is a custom validation to Validate publication year to ensure it's a positive integer and not in the future
+        model= Book
+        fields= '__all__'
     def validate_publication_year(self, value):
         current_year = datetime.now().year
-        if value < 0 or value > current_year:
-            raise serializers.ValidationError("Publication year must be a positive integer and not in the future.")
+        if value > current_year:
+            raise serializers.ValidationError("Publication year cannot be in the future.")
         return value
-       
-
-class AuthorSerializer(serializers.ModelSerializer):
-    # This is a nested serializer to include related books in the Author representation
-    books = BookSerializer(many=True, read_only=True)  # Nested serializer for related books
+class AutherSerializer(serializers.ModelSerializer):
+    books = BookSerializer(many=True, read_only=True)
     class Meta:
         model = Author
-        fields = ['id', 'name', 'books'] # Include 'id' for unique identification  
+        fields = ['name', 'books']
+         
