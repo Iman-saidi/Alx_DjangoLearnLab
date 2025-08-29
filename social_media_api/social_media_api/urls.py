737ff708d93d3b95c729f1f56841ef2a02f8d7be
Iman-saidi <summeiyaiman@gmail.com>
2025-08-30 02:34:29 +0300

    social_media_api

--- social_media_api/social_media_api/urls.py
+++ social_media_api/social_media_api/urls.py
@@ -19,4 +19,7 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/v1/accounts/', include('accounts.urls')),
+    path('api/v1/', include('posts.urls')),
+    path('api-auth/', include('rest_framework.urls')), # new
 ]
