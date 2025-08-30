bbed84cf401913f320c404b2024f04ec89ff1888
Iman-saidi <summeiyaiman@gmail.com>
2025-08-30 14:40:17 +0300

    social_media_api

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -79,6 +79,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'USER': 'yourdbuser',
+        'PASSWORD': 'yourdbpassword',
+        'HOST': 'localhost',  # Use production database host here
+        'PORT': '5432',
     }
 }
 
